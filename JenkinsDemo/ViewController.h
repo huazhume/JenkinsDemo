6f1c6b4b0650eded5b44aaa74d3d6cee425e0bf7
ygzhang <zhangyonggang@meiyuxiuxiu.com>
2018-08-30 18:14:24 +0800

    Initial Commit

---
+++ JenkinsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsDemo
+//
+//  Created by xiaobai zhang on 2018/8/30.
+//  Copyright © 2018年 xiaobai zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JenkinsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JenkinsDemo
+//
+//  Created by xiaobai zhang on 2018/8/30.
+//  Copyright © 2018年 xiaobai zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
